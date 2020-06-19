c480e928de1a1f2512db10b720ca18854fd91a2c
yinspark <675131787@qq.com>
2020-06-19 14:24:49 +0800

    展示使用手动更新和自动更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Awesome, welcome to Your Vue.js App"/>
   </div>
 </template>
 
